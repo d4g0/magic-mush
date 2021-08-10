ef9bb0bc5ea0213747cce4028700f5aefd576864
d4g0 <tocarralero@gmail.com>
2021-08-10 16:47:59 -0400

    baseline

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <div className='bg-gray-100 pb-10'>
+      <Head>
+        <title>Magic Mush</title>
+        <meta name='description' content='' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className='min-h-screen w-full text-on-surface'>
+        <div>page comming</div>
+      </main>
+
+      <footer className=''></footer>
+    </div>
+  );
+}
